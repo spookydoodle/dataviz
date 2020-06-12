135f8220149fd956e38e54adc96b8f4ff532fca2
kxkaro <kar.krajewska@gmail.com>
2020-06-12 11:36:37 +0200

    Updates

---
+++ precache-manifest.2eb515ad37cca9aec94b71f8b018a603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce5f00b64c081ff23984809d0da2485",
+    "url": "/dataviz/index.html"
+  },
+  {
+    "revision": "e4fbc8fad3b3ef27eb9f",
+    "url": "/dataviz/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c00f3c7840da279cb638",
+    "url": "/dataviz/static/js/2.c42199b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dataviz/static/js/2.c42199b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fbc8fad3b3ef27eb9f",
+    "url": "/dataviz/static/js/main.a2c9252b.chunk.js"
+  },
+  {
+    "revision": "e541e2861ce4a1d5a177",
+    "url": "/dataviz/static/js/runtime-main.cec0987b.js"
+  }
+]);
